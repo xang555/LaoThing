e80af025b2bed161b44e7cc9b115a942d84a6123
xang <xang.ultimate@gmail.com>
2017-04-06 13:55:08 +0700

    application model

--- smartSwitch/src/smartSwitch/smartSwitch.ino
+++ smartSwitch/src/smartSwitch/smartSwitch.ino
@@ -6,7 +6,7 @@
 #include <ESP8266WebServer.h>
 #include <WiFiUdp.h>
 #include <TimeLib.h>
-
+#include "FirebasePath.h"
 // Set firebae information
 #define FIREBASE_HOST "laothing-d014b.firebaseio.com"
 #define FIREBASE_AUTH "0twt7Oi5P0bJ55QSVZenjAgJuRXrXNbwc8YuR5ZT"
@@ -18,37 +18,6 @@
 
 String WIFI_SSID = "NODEMCU";
 String WIFI_PASSWORD = "nodemcu";
-
-const String DEVICE_ID = "837548734"; //set DEVICE ID
-
-const String uplink_path = DEVICE_ID + "/active/uplink"; // set uplink path
-const String ack_path = DEVICE_ID + "/active/ack"; // set ack path
-const String status_channel_one_path = DEVICE_ID + "/status/L1"; // set channle one path
-const String status_channel_two_path = DEVICE_ID + "/status/L2"; // set channle two path
-const String status_channel_three_path = DEVICE_ID + "/status/L3"; // set channle three path
-const String status_channel_four_path = DEVICE_ID + "/status/L4"; // set channle four path
-
-const String scheduler_state_channel_one_path = DEVICE_ID + "/scheduler/L1/state"; // state L1
-const String scheduler_status_channel_one_path = DEVICE_ID + "/scheduler/L1/status"; // set scheduler status change new one
-const String scheduler_time_hour_channel_one_path = DEVICE_ID + "/scheduler/L1/time/hour";// set scheduler huor change new one
-const String scheduler_time_minute_channel_one_path = DEVICE_ID + "/scheduler/L1/time/minute";// set scheduler minute change new one
-
-const String scheduler_state_channel_tow_path = DEVICE_ID + "/scheduler/L2/state"; // state L2
-const String scheduler_status_channel_tow_path = DEVICE_ID + "/scheduler/L2/status"; // set scheduler status change new tow
-const String scheduler_time_hour_channel_tow_path = DEVICE_ID + "/scheduler/L2/time/hour";// set scheduler huor change new tow
-const String scheduler_time_minute_channel_tow_path = DEVICE_ID + "/scheduler/L2/time/minute";// set scheduler minute change new tow
-
-const String scheduler_state_channel_three_path = DEVICE_ID + "/scheduler/L3/state"; // state L3
-const String scheduler_status_channel_three_path = DEVICE_ID + "/scheduler/L3/status";// set scheduler status change new three
-const String scheduler_time_channel_three_path = DEVICE_ID + "/scheduler/L3/time";// set scheduler time change new three
-const String scheduler_time_hour_channel_three_path = DEVICE_ID + "/scheduler/L3/time/hour";// set scheduler huor change new three
-const String scheduler_time_minute_channel_three_path = DEVICE_ID + "/scheduler/L3/time/minute";// set scheduler minute change new three
-
-const String scheduler_state_channel_four_path = DEVICE_ID + "/scheduler/L4/state"; // state L4
-const String scheduler_status_channel_four_path = DEVICE_ID + "/scheduler/L4/status";// set scheduler status change new four
-const String scheduler_time_channel_four_path = DEVICE_ID + "/scheduler/L4/time";// set scheduler time change new four
-const String scheduler_time_hour_channel_four_path = DEVICE_ID + "/scheduler/L4/time/hour";// set scheduler huor change new four
-const String scheduler_time_minute_channel_four_path = DEVICE_ID + "/scheduler/L4/time/minute";// set scheduler minute change new four
 
 
 static uint8_t state_mode = 0; // 0 is autoconct ; 1 is setting mode
@@ -888,4 +857,3 @@ long getTimeNow(){
   return ntp_time;
 
 } // get timestramp
-
