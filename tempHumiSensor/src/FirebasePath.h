aa3393b223dd4f154193b9706655ffeda9472375
xang <xang.ultimate@gmail.com>
2017-05-08 16:09:13 +0700

    temp and humi sensor

---
+++ tempHumiSensor/src/FirebasePath.h
@@ -0,0 +1,9 @@
+const String DEVICE_ID = "6357854354635200000"; //set DEVICE ID
+
+const String uplink_path = DEVICE_ID + "/active/uplink"; // set uplink path
+const String ack_path = DEVICE_ID + "/active/ack"; // set ack path
+
+const String update_time_unit_hour_path = DEVICE_ID+"/sensor/values/time/hour";
+const String update_time_unit_minute_path = DEVICE_ID+"/sensor/values/time/minute";
+const String humi_value_path = DEVICE_ID+"/sensor/values/hum/val";
+const String temp_value_path = DEVICE_ID+"/sensor/values/temp/val";
