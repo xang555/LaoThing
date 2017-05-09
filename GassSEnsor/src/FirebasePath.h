b118401b859259d6177d884149d1fcccf2c99522
xang <xang.ultimate@gmail.com>
2017-05-09 14:26:20 +0700

    code gass sensor

---
+++ GassSEnsor/src/FirebasePath.h
@@ -0,0 +1,6 @@
+const String DEVICE_ID = "635785435463527894"; //set DEVICE ID
+
+const String uplink_path = DEVICE_ID + "/active/uplink"; // set uplink path
+const String ack_path = DEVICE_ID + "/active/ack"; // set ack path
+const String fcm_server = "http://192.168.43.245:3000/homething/fcm";
+const String fcm_server_key = "eyJ1YWRtaW4iOiJhZG1pbiIsInBhZG1pbiI6ImFkbWluIiwiaWF0IjoxNDkyNzUyOTAwLCJleHAiOjE0OTI4MzkzMDB9";
