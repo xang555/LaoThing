2ef77ca30a9cbb1acf4292206e2bbd4946a286ea
xang <xang.ultimate@gmail.com>
2017-07-12 19:19:38 +0700

    add homething code

--- GassSEnsor/src/FirebasePath.h
+++
@@ -1,5 +0,0 @@
-const String DEVICE_ID = "1234567897878"; //set DEVICE ID
-
-const String uplink_path = DEVICE_ID + "/active/uplink"; // set uplink path
-const String ack_path = DEVICE_ID + "/active/ack"; // set ack path
-const String alert_listener_path = "alert_listener";
--- SmartAlarm/src/FirebasePath.h
+++
@@ -1,6 +0,0 @@
-const String DEVICE_ID = "1234567897895"; //set DEVICE ID
-
-const String uplink_path = DEVICE_ID + "/active/uplink"; // set uplink path
-const String ack_path = DEVICE_ID + "/active/ack"; // set ack path
-
-const String alert_path = DEVICE_ID+"/alert";
